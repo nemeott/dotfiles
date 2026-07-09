d6991fb8e700f2e218f6be2a674d9ce5a04a14fe
nemeott <123220311+nemeott@users.noreply.github.com>
2026-07-09 15:01:45 -0400

    Add OnlyOffice, downgrade Zen due to tabs not loading issues

--- nixos-config/modules/packages/productivity.nix
+++ nixos-config/modules/packages/productivity.nix
@@ -4,6 +4,7 @@
   environment.systemPackages = with pkgs; [
     obsidian
     libreoffice-fresh
+    onlyoffice-desktopeditors
     kdePackages.okular # PDF viewer
     pdfsam-basic # Edit PDFs
   ];
